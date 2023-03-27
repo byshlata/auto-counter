523795492ac789896c57d4b78be3b44a29a5213b
Aliaksandr <byshlata@yandex.ru>
2023-03-27 19:07:53 +0300

    finished app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <div class="pageContainer">
+      <div v-for="key in counters" :key="key">
+        <counter-time/>
+      </div>
+
+      <button class="btn" @click="addCounter">
+        <div class="cross">
+          <div class="line"/>
+          <div class="line lineVer"/>
+        </div>
+      </button>
+    </div>
+
+  </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue';
+import counterTime from '@/components/counter/counter-time.vue';
 
-@Options({
-  components: {
-    HelloWorld,
+export default defineComponent({
+  components: { counterTime },
+  data() {
+    return {
+      counters: [{ key: 0 }],
+    };
   },
-})
-export default class App extends Vue {}
+
+  methods: {
+    addCounter() {
+      this.counters.push({ key: this.counters.length });
+    },
+  },
+});
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
+
+@font-face {
+  font-family: "Gotham Pro";
+  src: local("Gotham Pro"),
+  url("@/assets/fonts/gothampro_light.ttf") format("truetype");
+}
+
+.app {
+  padding: 40px 20px 0 20px;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #E5E5E5;
+}
+
+.pageContainer {
+  display: flex;
+  align-items: start;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.btn {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 255px;
+  height: 120px;
+  border: none;
+  background-color: #696969;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.cross {
+  position: relative;
+  width: 20px;
+  height: 20px;
+}
+
+.line {
+  position: absolute;
+  height: 100%;
+  width: 3px;
+  top: 0;
+  right: 50%;
+  background-color: #9E9E9E;
+}
+
+.lineVer {
+  transform: rotate(-90deg);
+}
+
 </style>
--- src/components/counter/counter-time.vue
+++ src/components/counter/counter-time.vue
@@ -1,26 +1,22 @@
 <template>
-  <div id="container">
-
-    <div>{{ hour ? hour : '' }}</div>
-
-    <div>{{ minute ? minute : '' }}</div>
-
-    <div>{{ second }}</div>
+  <div class="counterBody">
+    <div v-bind:class="{ numberWrapper__active: isWork && !isPause, numberWrapper: true }">
+      <div class="numberItem">{{ hour ? `${hour}:` : '' }}</div>
+      <div class="numberItem">{{ minute ? `${minute}:` : '' }}</div>
+      <div class="numberItem">{{ second }}</div>
+    </div>
+    <div class="btnContainer">
+      <button @click="clickButton"
+              v-bind:class="{ playBtn: !isWork || isPause,
+             pauseBtn: isWork && !isPause, btnWrapper: true  } "/>
+      <button @click="reset"
+              v-bind:class="{ resetBtn__active: isWork && !isPause, resetBtn: true }"/>
+    </div>
   </div>
-  <div id="container">
-
-    <button @click="start">Start</button>
-
-    <button @click="pause">Pause</button>
-
-    <button @click="reset">Stop</button>
-  </div>
-
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Counter from './utils/Counter';
 
 export default defineComponent({
 
@@ -30,37 +26,134 @@ export default defineComponent({
       minute: 0,
       hour: 0,
       isPause: false,
-      Counter: new Counter(),
+      isWork: false,
     };
   },
   methods: {
-    changeTime(second: number, minute: number, hour: number): void {
-      this.second = second;
-      this.minute = minute;
-      this.hour = hour;
+    play() {
+      this.isPause = false;
+      this.isWork = true;
+      this.startCounter();
     },
 
-    start(): void {
-      this.Counter.subscribe(this.changeTime);
-      this.Counter.start();
+    pause() {
+      this.isPause = true;
     },
 
-    reset(): void {
-      this.Counter.reset();
+    reset() {
+      this.isWork = false;
+      this.isPause = true;
+      this.second = 0;
+      this.minute = 0;
+      this.hour = 0;
     },
 
-    pause(): void {
-      this.Counter.pause();
+    startCounter() {
+      const countedSecond = setInterval(() => {
+        if (this.isPause) {
+          clearTimeout(countedSecond);
+        }
+
+        this.second += 1;
+
+        if (this.second === 60) {
+          this.second = 0;
+          this.minute += 1;
+        }
+
+        if (this.minute === 60) {
+          this.minute = 0;
+          this.hour += 1;
+        }
+
+        if (this.hour === 24) {
+          this.reset();
+        }
+      }, 1000);
+    },
+
+    clickButton(): void {
+      if (this.isWork && !this.isPause) {
+        this.pause();
+        return;
+      }
+      this.play();
     },
   },
 });
 </script>
 
 <style>
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
+
+.counterBody {
+  display: flex;
+  align-content: center;
+  flex-direction: column;
+  width: 255px;
+  height: 120px;
+  background-color: #696969;
+  margin: 5px;
+}
+
+.numberWrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 22px;
+  font-family: "Gotham Pro", Helvetica, Arial;
+  font-weight: 400;
+  height: 50%;
+  width: 100%;
+  color: #9E9E9E;
+  border-bottom: 1px solid #9E9E9E;
+  background: inherit;
+}
+
+.numberWrapper__active {
+  color: #ffffff;
+  border-bottom: 1px solid #ffffff;
+}
+
+.numberItem {
+  background: inherit;
+}
+
+.btnContainer {
+  height: 50%;
+  display: flex;
+  align-items: center;
+  padding: 0 70px;
+  justify-content: space-between;
+}
+
+.btnWrapper {
+  position: relative;
+  width: 20px;
+  height: 20px;
+  border: none;
+  background-color: inherit;
+}
+
+.playBtn {
+  background: url("@/assets/icon/icon-play.svg") no-repeat;
+  cursor: pointer;
+}
+
+.resetBtn {
+  width: 20px;
+  height: 20px;
+  background-color: #9E9E9E;
+  border: none;
+  cursor: pointer;
+}
+
+.resetBtn__active {
+  background-color: #ffffff;
+}
+
+.pauseBtn {
+  background: url("@/assets/icon/icon-pause.svg") center no-repeat;
+  cursor: pointer;
 }
 
 </style>
